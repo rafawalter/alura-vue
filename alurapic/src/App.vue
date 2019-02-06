fccc1f67e6a669e98103aabd3a162777737d7f2d
Rafael Fraga Walter <rafael.walter@bndes.gov.br>
2019-02-06 11:28:40 -0200

    06 Single Page Application e criação de rotas

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,82 +1,43 @@
 <template>
   <div class="corpo">
-    <h1 class="centralizado">{{ titulo }}</h1>
+    <meu-menu :rotas="routes"/>
 
-    <input
-      type="search"
-      class="filtro"
-      @input="filtro = $event.target.value"
-      placeholder="filtre pelo título da foto"
-    >
-    {{ filtro }}
-    <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
-        <meu-painel :titulo="foto.titulo">
-          <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
-        </meu-painel>
-      </li>
-    </ul>
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
-import Painel from "./components/shared/painel/Painel";
-import ImagemResponsiva from "./components/shared/imagem-responsiva/ImagemResponsiva";
+import { routes } from "./routes";
+import Menu from "./components/shared/menu/Menu";
 
 export default {
   components: {
-    "meu-painel": Painel,
-    "imagem-responsiva": ImagemResponsiva
+    "meu-menu": Menu
   },
   data() {
     return {
-      titulo: "Alurapic",
-      fotos: [],
-      filtro: ""
+      routes
     };
-  },
-
-  computed: {
-    fotosComFiltro() {
-      if (this.filtro) {
-        let exp = new RegExp(this.filtro.trim(), "i");
-        return this.fotos.filter(foto => exp.test(foto.titulo));
-      } else {
-        return this.fotos;
-      }
-    }
-  },
-
-  created() {
-    this.$http
-      .get("http://localhost:3000/v1/fotos")
-      .then(res => res.json())
-      .then(fotos => (this.fotos = fotos), err => console.log("ERRO", err));
   }
 };
 </script>
 
 <style>
-.centralizado {
-  text-align: center;
-}
-
 .corpo {
   font-family: Helvetica, sans-serif;
   margin: 0 auto;
   width: 96%;
 }
 
-.lista-fotos {
-  list-style: none;
+.pagina-enter-active,
+.pagina-leave-active {
+  transition: opacity 0.3s;
 }
 
-.lista-fotos .lista-fotos-item {
-  display: inline-block;
-}
-
-.filtro {
-  display: block;
-  width: 100%;
+.pagina-enter,
+.pagina-leave-active {
+  opacity: 0;
 }
 </style>
